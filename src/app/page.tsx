6a267dd3f129378c77853146dc628ef98d610a93
Dishant Yadav <dishantrao11@gmail.com>
2024-05-19 12:47:41 +0530

    done with auth

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
+import React from "react";
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs";
+import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs";
+
+export default function Home() {
+  useEffect(() => {
+redirect('/api/auth/login?post_login_redirect_url=/dashboard')
+  }, [])
+  
+  return (
+    <>
+    </>
+  );
+}
